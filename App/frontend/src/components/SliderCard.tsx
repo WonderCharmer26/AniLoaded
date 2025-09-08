1e442e8dc4ab76df25a0becd2fae32cfba5a2476
Kyle Francis <sjack421@gmail.com>
2025-09-08 11:14:38 -0400

    Establishing new branch
    - basic set up for slider component set up

--- App/frontend/src/components/SliderCard.tsx
+++ App/frontend/src/components/SliderCard.tsx
@@ -1,6 +1,7 @@
 // Slider Card for the Hompage Component
 
-import { Link } from "react-router-dom"; // used for the routing links to the different anime popular anime pages
+// import { Link } from "react-router-dom"; // used for the routing links to the different anime popular anime pages
+import { useQuery } from "@tanstack/react-query";
 
 // TODO: make a interface for the slider card component, the slider will have the data from supabase to be displayed
 interface SliderCardI {
@@ -11,11 +12,18 @@ interface SliderCardI {
   description?: string; // optional: might make a description to be displayed on hover
 }
 
+// TODO: connect to the backend,make function with axios to get the anime picks from the storage in the db
+const { data, isLoading, error } = useQuery({
+  queryKey: ["slider"],
+});
+
 export const SliderCard = () => {
   // TODO: connect to the backend, set up a query to get the top anime pictures for the slider (no use mutate needed to refetch)
   return (
-    <div className="flex items-center justify-center w-full bg-[#0C0C0C] h-[38.875rem]">
-      Slider
+    <div className="flex items-center justify-center p-2 w-full rounded-2xl bg-[#0C0C0C] h-[38.875rem]">
+      <div className="flex items-center justify-center w-full rounded-2xl bg-blue-950 h-full">
+        Slider Card
+      </div>
     </div>
   );
 };
---
+++ App/frontend/src/services/getMainPagePhotos.ts
@@ -0,0 +1,20 @@
+// functions to get the data from supabase for the main page
+// TODO: add in toast notifications, download the library
+
+// imports
+import { supabase } from "./supabaseConnection";
+
+// slider data fetching function
+const getSliderPhotos = async () => {
+  const { data, error } = await supabase.storage
+    .from("Main Page")
+    .list("Carousel");
+
+  if (error) {
+    console.log(error);
+  }
+
+  // otherwise return the data
+  // map through the data of the path names to get use the url to display the images
+  return data?.map((image) => {});
+};
