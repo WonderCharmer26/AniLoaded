73d82441a6d13c096ab5d57a4ad8b04310abf6ac
Kyle Francis <sjack421@gmail.com>
2025-09-07 23:43:17 -0400

    Added supabase connection & installed supabase lib
    - set up the connection so that we have access to the database
    - installed supabse to the project

---
+++ App/frontend/src/services/supabaseConnection.ts
@@ -0,0 +1,21 @@
+// connection to the supabase data base is handled in here
+
+// imports
+import { createClient } from "@supabase/supabase-js";
+
+// bring in the .env variables
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+// check if the variables are brought in properly
+if (!supabaseUrl) {
+  throw new Error("Missing supabaseUrl variable");
+}
+
+// check if the variables are brought in properly
+if (!supabaseKey) {
+  throw new Error("Missing supabaseKey variable");
+}
+
+// export the supabase client to be used in other files if needed
+export const supabase = createClient(supabaseUrl, supabaseKey);
