a6c8adfc865129abae73ebbf1c1aa1398150bb91
Kyle Francis <sjack421@gmail.com>
2025-09-16 11:32:43 -0400

    Reformated Schema, Added Carousel Titles, Set Tables
    - set up the table to get the data from the database to load the images
    - Added the title to the images on the frontend (still have to style and
    finish rest of set up)
    - reformatted the schemas and made the organization cleaner for the
    codebase

--- App/frontend/src/components/Carousel.tsx
+++ App/frontend/src/components/Carousel.tsx
@@ -5,7 +5,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCarouselPhotos } from "../services/getMainPagePhotos";
 import { ClockLoader } from "react-spinners";
-import { CarouselI } from "../services/getMainPagePhotos"; // interface for the carousel data gotten from supabase
+import { CarouselI } from "../schemas/CarouselSchema";
 import { useState } from "react";
 
 export const CarouselComponent = () => {
@@ -17,16 +17,16 @@ export const CarouselComponent = () => {
   // make an array of objects for the names of the shows, might make a table in the database to fetch the names to display on the frontend
 
   // image for slider retrived as mounting
-  const { data, isLoading, error } = useQuery<CarouselI[]>({
+  const { data, isLoading, isFetched, error } = useQuery<CarouselI[]>({
     queryKey: ["slider"],
     queryFn: getCarouselPhotos, // function to get handle getting the data from supabase to load into the slider
   });
 
   return (
     <div className="flex items-center justify-center relative p-2 w-full rounded-2xl bg-[#0C0C0C] h-[38.875rem]">
-      <div className="flex flex-row items-center justify-center overflow-hidden gap-1 w-full rounded-2xl bg-blue-950 h-full">
+      <div className="flex flex-row items-center justify-center overflow-hidden gap-1 w-full rounded-2xl bg-gray-950 h-full">
         {/* error handling */}
-        {error && <div>There was an error loading the image</div>}
+        {error && isFetched && <div>There was an error loading the image</div>}
         {/* no image found */}
         {!isLoading && (!data || data.length === 0) && (
           <p className="text-white">No images found</p>
@@ -37,15 +37,20 @@ export const CarouselComponent = () => {
             <ClockLoader color="white" />
           </>
         )}
-        {/* Show the images here */}
-        {data && data.length > 0 && (
+        {/* Show the images here if the data is fetched and there are images */}
+        {isFetched && data && data.length > 0 && (
           // add in the names for each of the anime and genres
-          <img
-            key={data[activeIndex].name}
-            src={data[activeIndex].url}
-            alt={data[activeIndex].name}
-            className="w-full h-full object-cover rounded-2xl bg-black transition-opacity duration-500"
-          />
+          <div className="flex items-center justify-center w-full h-full relative">
+            <div className="absolute z-1">
+              <h1>{data[activeIndex].title.toUpperCase()}</h1>
+            </div>
+            <img
+              key={data[activeIndex].id}
+              src={data[activeIndex].url}
+              alt={data[activeIndex].title}
+              className="w-full h-full scale-[1.01] object-cover rounded-2xl bg-black transition-opacity duration-500"
+            />
+          </div>
         )}
       </div>
       {/* TODO: add in selectors to shuffle through the images */}
--- App/frontend/src/services/getMainPagePhotos.ts
+++ App/frontend/src/services/getMainPagePhotos.ts
@@ -3,13 +3,8 @@
 
 // imports
 import { supabase } from "./supabaseConnection";
-
-// interface for carousel data
-export interface CarouselI {
-  // all the data will be fetched from supabase
-  name: string;
-  url: string;
-}
+import { CarouselI } from "../schemas/CarouselSchema";
+// import { CarouselTitlesI } from "../schemas/CarouselSchema";
 
 // slider data fetching function
 export const getCarouselPhotos = async (): Promise<CarouselI[]> => {
@@ -17,57 +12,34 @@ export const getCarouselPhotos = async (): Promise<CarouselI[]> => {
   const bucket = import.meta.env.VITE_SUPABASE_BUCKET!;
   const folder = import.meta.env.VITE_SUPABASE_FOLDER!;
 
-  //NOTE: check the buckets (remove this later)
-  const { data: test, error: testError } = await supabase.storage.listBuckets();
-  console.log("test bucket data:", test);
-  console.log("test bucket error:", testError);
-
-  // NOTE: Testing to see if the env variables are working (remove this later)
-
-  console.log("bucket", bucket);
-  console.log("folder", folder);
-  console.log("full path:", `${bucket}/${folder}`);
-
-  // get the data from supabase
-  const { data: files, error } = await supabase.storage
-    .from(bucket)
-    .list(`${folder}`);
-
-  // NOTE: testing the response from supabase (remove this later)
-  console.log("files data:", files);
-  console.log("error data:", error);
+  // fetch the title, genre, and storage_path from the database
+  const { data: carouselImgInfo, error: carouselImgInfoError } = await supabase
+    .from("carousel_images")
+    .select("id, storage_path, title, genre")
+    .order("id", { ascending: true });
 
   // NOTE: if there is an error, return the results (remove this later)
-  if (error) {
-    console.log("error listing files:", error);
+  if (carouselImgInfoError) {
+    console.log("error getting carousel image info:", carouselImgInfoError);
     return [];
   }
 
-  // if the files are empty, return an empty array
-  if (!files || files.length === 0) {
-    console.log("no files found in folder", folder);
-    return [];
-  }
-
-  // NOTE: otherwise the files are found and log the information for proof of concept (remove this later)
-
-  console.log("files were found:", files.length);
-  console.log(
-    "File names:",
-    files.map((file) => file.name),
-  );
+  // log to see if the url is correct
+  console.log(carouselImgInfo.map((row) => row.storage_path));
 
   // generate urls with the full folder path
-  return files.map((file) => {
+  return carouselImgInfo.map((row) => {
     // gets the public url for each image in the bucket
-    const { data } = supabase.storage
+    const { data: urlData } = supabase.storage
       .from(bucket)
-      .getPublicUrl(`${folder}/${file.name}`);
+      .getPublicUrl(`${folder}/${row.storage_path}`);
 
     // returns an object with the name and the url
     return {
-      name: file.name,
-      url: data.publicUrl,
+      id: row.id,
+      title: row.title,
+      url: urlData.publicUrl,
+      genre: row.genre,
     };
   });
 };
