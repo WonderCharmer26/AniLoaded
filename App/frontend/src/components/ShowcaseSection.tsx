ec9a6ee4f468f007beb8697b247be3d365f45e48
Kyle Francis <sjack421@gmail.com>
2025-09-18 10:41:30 -0400

    Added spacing to the bottom of the ShowcaseSection to space the cards
    added mb-3 to ShowcaseSection to add a bit of spacing

--- App/frontend/src/components/ShowcaseSection.tsx
+++ App/frontend/src/components/ShowcaseSection.tsx
@@ -9,7 +9,7 @@ export const ShowcaseSection: React.FC<ShowcaseSectionI> = ({
   return (
     <>
       {" "}
-      <div className="flex flex-row items-center mt-8">
+      <div className="flex flex-row items-center mt-8 mb-3">
         <hr className="bg-white opacity-20 w-full h-[0.3px]"></hr>
         <div className="flex flex-row items-center ">
           <h2 className="text-white font-bold w-[150px]">{sectionName}</h2>
