e72f251e8414695e9ac136bb1545958069017e40
Kyle Francis <sjack421@gmail.com>
2025-09-20 21:14:31 -0400

    Set up the trending route for the backend
    - set up the groundwork for the backend route

--- App/backend/main.py
+++ App/backend/main.py
@@ -5,6 +5,7 @@
 # imports
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import httpx  # for handling the requests on the backend to get data from the Ani-list api
 
 # from pydantic import BaseModel (might use, handling BaseModel in schema folder)
 
@@ -28,3 +29,49 @@ app.add_middleware(
 @app.get("/")
 def root():
     return {"test_message": "This is a test message to show the backend is working"}
+
+
+# route to get the trending anime from Ani-list
+@app.get("/trending_anime")
+async def get_trending_anime():
+    # set up the query to get the trending anime
+    query = """
+
+    query ($perPage: Int, $page: Int) {
+        Page(page: $page, perPage: $perPage) {
+           media(type: ANIME, sort: TRENDING_DESC) {
+               id
+               title {
+                romanji
+                english
+                native
+               }      
+               episodes
+               coverImage
+               large
+           } 
+        }        
+    }"""
+
+    # variables that can be used to get the amount of items that I want
+    variables = {
+        "page": 1,
+        "perPage": 10,
+    }  # set the amount items per page, might tweak to have more later on
+
+    # send the query to the Ani-list api
+    async with httpx.AsyncClient() as client:  # use httpx to make the request to handle everything about the request
+        # make a post request to the Ani-list api
+        response = await client.post(
+            "https://graphql.anilist.co",
+            # pass in the query and the varibales to get the items and the amount of items we want fetched
+            json={"query": query, "variables": variables},
+            # set the content type to json
+            headers={"Content-Type": "application/json"},
+        )
+
+    # package the data in a variable to send
+    data = response.json()
+
+    # return the data
+    return data
